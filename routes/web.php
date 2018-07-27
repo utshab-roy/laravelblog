b8dc8825bc35812e8b507326a18477c4f8755cd5
Utshab Roy <utshab.roy@gmail.com>
2018-07-28 02:17:55 +0600

    learned DB connect, query builder basic route

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,24 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function (){
     return view('welcome');
 });
+
+//showing all the task as list
+Route::get('/tasks', function () {
+
+    $tasks = DB::table('tasks')->get();
+//    return $tasks; // this will give the JSON formatted data to the route 
+    return view('tasks.index', compact('tasks'));
+});
+
+//showing individual task in h1 tag
+Route::get('/tasks/{task}', function ($id) {
+
+    $tasks = DB::table('tasks')->find($id);
+
+//    return $tasks;
+    return view('tasks.show', compact('tasks'));
+});
+
